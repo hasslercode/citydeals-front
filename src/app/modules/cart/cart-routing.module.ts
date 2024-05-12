f96603bec322bd2939b90e988620edeb81b89313
hassler <hisaac@bancolombia.com.co>
2024-05-12 13:57:47 -0500

    add cart

--- src/app/modules/cart/cart-routing.module.ts
+++ src/app/modules/cart/cart-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterCartComponent } from './ui/pages/register-cart/register-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterCartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/cart/cart.module.ts
+++ src/app/modules/cart/cart.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CartRoutingModule } from './cart-routing.module';
+import { RegisterCartComponent } from './ui/pages/register-cart/register-cart.component';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterCartComponent
+  ],
   imports: [
     CommonModule,
-    CartRoutingModule
+    CartRoutingModule,
+    SharedModule
   ]
 })
 export class CartModule { }
